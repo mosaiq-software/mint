f1e4ed47e758fd65b3af2709aeb9f2895c08f686
samranda <samrandaofficial@gmail.com>
2025-10-04 20:57:05 -0400

    Added fonts and global styling

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { mount } from 'svelte'
 import App from './App.svelte'
+import './assets/styles/reset.css'
+import './assets/styles/global.css'
+import './assets/styles/theme.css'
 
 const app = mount(App, {
   target: document.getElementById('app')!,
