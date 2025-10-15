a41b4b5f7697149e88e1445b10927be193ff1af6
samranda <samrandaofficial@gmail.com>
2025-10-15 10:35:07 -0400

    Added data structure for tracking layer changes

--- src/lib/scripts/ui.svelte.ts
+++ src/lib/scripts/ui.svelte.ts
@@ -1,13 +1,19 @@
 import type { DocumentID, Color } from "./docs.svelte";
-import type { LayerID } from "./layer";
+import type { LayerID, Layer } from "./layer";
 
 export const modes = ['select', 'draw', 'erase', 'text'] as const;
 export type Mode = typeof modes[number];
+
+interface Action {
+    oldLayer: Layer | null;
+    newLayer: Layer | null;
+}
 
 interface UI {
     mode: Mode;
     selectedDocument: DocumentID | null;
     selectedLayers: Record<DocumentID, LayerID[]>;
+    actions: Record<DocumentID, Action[]>;
     foregroundColor: Color,
     backgroundColor: Color,
 }
@@ -16,6 +22,7 @@ const ui: UI = $state({
     mode: modes[0],
     selectedDocument: null,
     selectedLayers: {},
+    actions: {},
     foregroundColor: { r: 0, g: 0, b: 0, a: 1 },
     backgroundColor: { r: 255, g: 255, b: 255, a: 1 },
 });
