fefcf9ae61cd8a6520a8707859d28316b0ae7631
asantagata <asantagata225@gmail.com>
2025-10-23 18:22:48 -0400

    warn on close

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,6 +4,8 @@
     import {IconButtonVisual} from "./ui";
     import ui from "../scripts/ui.svelte";
     import saveStatus from "../scripts/saveStatus.svelte";
+    import {Popover} from "melt/builders";
+
     const tabs = $derived.by(() => {
         const {selected, ...rest} = docs;
         return rest;
@@ -22,23 +24,34 @@
         delete docs[tab.id];
         delete saveStatus[tab.id];
     }
+
+    const deleteWarningPopover = new Popover();
+    let docUpForDeletion = $state<null | Document>(null);
 </script>
 
 {#if Object.keys(tabs).length > 0}
     <header class="header">
         {#each Object.values(tabs) as tab}
-            <div class="{docs.selected?.id === tab.id ? 'selected' : ''}">
+            <div class="{docs.selected?.id === tab.id ? 'selected' : ''} tab">
                 <button onclick={() => handleTabClick(tab)} class="name">
                     <span>{tab.name}</span>
                 </button>
-                <button onclick={() => handleTabDelete(tab)} class="close">
+                <button {...deleteWarningPopover.trigger} onclick={(e) => {
+                    console.log(saveStatus[tab.id]);
+                    if (saveStatus[tab.id] === 0) {
+                        handleTabDelete(tab);
+                    } else {
+                        docUpForDeletion = tab;
+                        deleteWarningPopover.trigger.onclick(e);
+                    }
+                }} class="close">
                     <IconButtonVisual
                             label="Close"
                             paddingSMd={true}
                     >
-                        <i class="x-wrapper" class:unsaved={saveStatus[tab.id] !== 0}>
+                        <div class="x-wrapper" class:unsaved={saveStatus[tab.id] !== 0}>
                             <X size={16} />
-                        </i>
+                        </div>
                     </IconButtonVisual>
                 </button>
             </div>
@@ -52,17 +65,54 @@
                 <Plus size={16} />
             </button>
         </div>
+        <div {...deleteWarningPopover.content} class="warning-menu">
+            <div {...deleteWarningPopover.arrow}></div>
+            <div>
+                {#if docUpForDeletion}
+                    <p>Are you sure you want to close <b>{docUpForDeletion.name}</b>?</p>
+                    <p>This document may have unsaved changes.</p>
+                    <button class="warn" onclick={() => {
+                        if (docUpForDeletion) {
+                            deleteWarningPopover.open = false;
+                            handleTabDelete(docUpForDeletion);
+                        }
+                    }}>Delete</button>
+                {/if}
+            </div>
+        </div>
     </header>
 {/if}
 
 <style>
+    .warn {
+        color: var(--c-fb-err);
+        text-align: left;
+        padding: 0 var(--s-sm);
+        cursor: pointer;
+        width: 100%;
+        text-align: center;
+    }
+
+    .warn:hover {
+        background: var(--c-sur);
+    }
+
+    .warning-menu {
+        background: var(--c-mid);
+        border: none;
+        padding: var(--s-sm);
+        border-radius: var(--s-sm);
+        box-shadow: 0px 0px 10px 0px var(--c-bg);
+        margin-left: var(--s-xs);
+    }
+
     .close:not(:hover) .unsaved {
         background: var(--c-txt);
         border-radius: 50%;
         transform: scale(0.5);
     }
 
-    div {
+    .tab {
         background: var(--c-sur);
         flex-grow: 1;
         cursor: pointer;
@@ -79,7 +129,7 @@
         cursor: pointer;
     }
 
-    div.selected button.name {
+    .tab.selected button.name {
         cursor: default;
     }
 
@@ -98,11 +148,11 @@
         text-overflow: ellipsis;
     }
 
-    div:first-child, div:hover, div:hover + div, div.selected, .selected + div {
+    .tab:first-child, .tab:hover, .tab:hover + .tab, .tab.selected, .selected + .tab {
         border-left-color: transparent;
     }
 
-    div:hover {
+    .tab:hover {
         background: var(--c-mid);
     }
 
@@ -110,12 +160,12 @@
         flex-grow: 0;
     }
 
-    div.selected, div.selected:hover {
+    .tab.selected, .tab.selected:hover {
         background: var(--c-bg);
         cursor: default;
     }
 
-    div.selected, div:hover {
+    .tab.selected, .tab:hover {
         border-top-left-radius: var(--s-md);
         border-top-right-radius: var(--s-md);
     }
