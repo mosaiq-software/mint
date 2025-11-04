d1791822ca9dd94a10a298e918c87f43237330d6
samranda <samrandaofficial@gmail.com>
2025-11-04 18:18:02 -0500

    Only create rectangle once dragged

--- src/lib/scripts/tools/rectangle.svelte.ts
+++ src/lib/scripts/tools/rectangle.svelte.ts
@@ -1,9 +1,10 @@
-import type { Tool } from '.';
+import type { PointerEventData, Tool } from '.';
 import { getSelectedRectangleLayer } from './utils/shape.svelte';
-import { createLayer } from '../layer';
+import { createLayer, type RectangleLayer } from '../layer';
 import type { Point } from '.';
 import docs from '../docs.svelte';
 import ui from '../ui.svelte';
+import { postAction } from '../action';
 
 const rectangle = $state({
     dragging: false,
@@ -14,7 +15,7 @@ const p = {
     initial: { x: 0, y: 0 } as Point,
 };
 
-const selected = $derived.by(getSelectedRectangleLayer);
+let selected = $derived.by(getSelectedRectangleLayer);
 
 export const rectangleTool: Tool = {
     name: 'rectangle',
@@ -22,48 +23,58 @@ export const rectangleTool: Tool = {
         rectangle.dragging = true;
         if (rectangle.action === 'create') {
             p.initial = {...data.c};
-
-            // create new rectangle layer and select it
-            if (!docs.selected || !ui.selected) return;
-            const newLayer = createLayer('rectangle', 'New Rectangle');
-            docs.selected.layers.push(newLayer);
-            ui.selected.selectedLayers = [ newLayer.id ];
+            if (ui.selected) ui.selected.selectedLayers = [];
         } else {
-            setAction();
+            setAction(data);
         }
     },
     onPointerMove: (data) => {
         if (rectangle.dragging) {
             if (rectangle.action === 'create') {
-                if (!selected) return;
+                if (p.initial === null) return;
+
                 const dx = data.c.x - p.initial.x;
                 const dy = data.c.y - p.initial.y;
+                const width = Math.abs(dx);
+                const height = Math.abs(dy);
 
-                selected.width = Math.abs(Math.abs(dx));
-                selected.height = Math.abs(Math.abs(dy));
-                selected.transform.matrix.e = dx < 0 ? data.c.x : p.initial.x;
-                selected.transform.matrix.f = dy < 0 ? data.c.y : p.initial.y;
+                if (!selected && width > 0 && height > 0) {
+                    // create new rectangle layer and select it
+                    if (!docs.selected || !ui.selected) return;
+                    const newLayer = createLayer('rectangle', 'New Rectangle');
+                    docs.selected.layers.push(newLayer);
+                    ui.selected.selectedLayers = [ newLayer.id ];
+                }
+
+                if (selected) {
+                    selected.width = width;
+                    selected.height = height;
+                    selected.transform.matrix.e = dx < 0 ? data.c.x : p.initial.x;
+                    selected.transform.matrix.f = dy < 0 ? data.c.y : p.initial.y;
+                }
             }
         } else {
-            setAction();
+            setAction(data);
         }
     },
     onPointerUp: (data) => {
         rectangle.dragging = false;
 
-        if (rectangle.action === 'create') {
+        if (rectangle.action === 'create' && selected) {
             ui.mode = 'select';
             rectangle.action = 'none';
+
+            postAction({
+                type: 'create',
+                layer: selected!,
+                position: docs.selected!.layers.length - 1
+            });
         }
     },
 };
 
-function setAction() {
-    if (selected) {
-        rectangle.action = 'none';
-    } else {
-        rectangle.action = 'create';
-    }
+function setAction(data: PointerEventData) {
+    rectangle.action = 'create';
 }
 
 export default rectangle;
