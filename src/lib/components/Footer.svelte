bb07f4bf82cc8d7614f971bcf9a481ddf43e42f3
asantagata <asantagata225@gmail.com>
2025-11-05 00:08:14 -0500

    expose canUndo and canRedo; populate footer with keybinds & instructions

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,16 +1,104 @@
 <script lang="ts">
+    import docs from "../scripts/docs.svelte";
+    import ui from "../scripts/ui.svelte";
+    import {SquareDashed, ArrowLeft, ArrowRight, ArrowUp, ArrowDown} from "@lucide/svelte";
+    import tabStatus from "../scripts/tabStatus.svelte";
 
-import docs from "../scripts/docs.svelte";
+    const isMacLike = /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgent);
+    const ctrl = isMacLike ? '⌘' : 'Ctrl';
 
+    const keybindConnector = " + ";
+    const keySVGSize = 12;
+
+    const selectedLayer = $derived(docs.selected?.layers.find(l => l.id === ui.selected?.selectedLayers[0]));
 </script>
 
 {#if docs.selected}
     <footer>
-        helloworld
+        {#if ui.mode === "select"}
+            {#if !selectedLayer}
+                <p>Click a layer to select</p>
+            {:else}
+                <p>Move <i><SquareDashed size={16} /></i> to move</p>
+                <p>Move <i class="handle r-handle"></i> to rotate</p>
+                <p>Move <i class="handle s-handle"></i> to resize</p>
+                <p>
+                    <kbd><ArrowLeft size={keySVGSize} /></kbd>
+                    <kbd><ArrowUp size={keySVGSize} /></kbd>
+                    <kbd><ArrowRight size={keySVGSize} /></kbd>
+                    <kbd><ArrowDown size={keySVGSize} /></kbd> to move
+                </p>
+                <p>
+                    <kbd>Shift</kbd>
+                    {keybindConnector}
+                    <kbd><ArrowLeft size={keySVGSize} /></kbd>
+                    <kbd><ArrowUp size={keySVGSize} /></kbd>
+                    <kbd><ArrowRight size={keySVGSize} /></kbd>
+                    <kbd><ArrowDown size={keySVGSize} /></kbd> to move further
+                </p>
+            {/if}
+        {:else if ui.mode === "draw"}
+            {#if selectedLayer?.type === "canvas"}
+                <p>Click and drag to draw</p>
+                <p><kbd>E</kbd> to erase</p>
+            {:else}
+                <p>Click to make a new canvas layer</p>
+            {/if}
+        {:else if ui.mode === "erase"}
+            {#if selectedLayer?.type === "canvas"}
+                <p>Click and drag to erase</p>
+                <p><kbd>D</kbd> to draw</p>
+            {:else}
+                <p>Select a canvas layer to erase</p>
+            {/if}
+        {:else if ui.mode === "text"}
+            {#if selectedLayer?.type === "text"}
+                <p>Type in layer to edit text</p>
+            {:else}
+                <p>Click to make a text layer</p>
+            {/if}
+        {/if}
+
+        {#if tabStatus[docs.selected.id].canUndo}
+            <p><kbd>{ctrl}</kbd>{keybindConnector}<kbd>Z</kbd> to undo</p>
+        {/if}
+        {#if tabStatus[docs.selected.id].canRedo}
+            <p><kbd>{ctrl}</kbd>{keybindConnector}<kbd>Y</kbd> to redo</p>
+        {/if}
+        {#if tabStatus[docs.selected.id].actionsSinceSave !== 0}
+            <p><kbd>{ctrl}</kbd>{keybindConnector}<kbd>S</kbd> to save</p>
+        {/if}
     </footer>
 {/if}
 
 <style>
+    .handle {
+        width: 10px;
+        height: 10px;
+        background: var(--c-txt);
+        border: 1px solid var(--c-bg);
+    }
+
+    .r-handle {
+        border-radius: 50%;
+    }
+
+    i, kbd {
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    p {
+        flex-shrink: 0;
+        white-space: nowrap;
+    }
+
+    p:not(:last-child)::after {
+        content: '•';
+        color: var(--c-off);
+        padding: 0 var(--s-sm);
+    }
+
     footer {
         display: flex;
         justify-content: start;
@@ -21,5 +109,6 @@ import docs from "../scripts/docs.svelte";
         overflow: auto;
         flex-shrink: 0;
         padding: var(--s-sm);
+        cursor: default;
     }
 </style>
