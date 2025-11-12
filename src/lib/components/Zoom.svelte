e12c66cf49f3a0db239b4f3570f48160177a11fb
asantagata <asantagata225@gmail.com>
2025-11-11 22:22:00 -0500

    zoom dropdown

--- src/lib/components/Zoom.svelte
+++ src/lib/components/Zoom.svelte
@@ -18,6 +18,17 @@
             ui.selected.zoom *= 1.1;
         }
     }
+
+    const zooms = [30, 50, 70, 80, 90, 100, 110, 120, 130, 150, 170, 200, 250, 300, 400, 500]
+
+    let dropdownZoom = $state(-1);
+
+    function setZoomFromDropdown() {
+        if (ui.selected) {
+            ui.selected.zoom = dropdownZoom;
+            dropdownZoom = -1;
+        }
+    }
 </script>
 
 {#if docs.selected}
@@ -27,7 +38,12 @@
                 <ZoomOut size={16} />
             </IconButtonVisual>
         </button>
-        {zoomStr}
+        <select bind:value={dropdownZoom} onchange={setZoomFromDropdown}>
+            {#each zooms as z}
+                <option value={z / 100}>{z}%</option>
+            {/each}
+            <option value={-1} selected>{zoomStr}</option>
+        </select>
         <button onclick={zoomIn}>
             <IconButtonVisual label="Zoom in">
                 <ZoomIn size={16} />
